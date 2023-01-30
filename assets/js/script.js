5a2ab43119c34bb88b1b5c8c8dab97ca4cdb0a9f
Diether Marcelo <miguelitossss199@gmail.com>
2023-01-30 10:54:29 +0800

    Fix responsive design and mobile nav item clicked

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,7 @@
-let mobile_close    = document.querySelector('.close');
-let mobile_nav      = document.querySelector('.mobile-nav');
-let burger          = document.querySelector('.burger-icon')
+let mobile_close        = document.querySelector('.close');
+let mobile_nav          = document.querySelector('.mobile-nav');
+let burger              = document.querySelector('.burger-icon');
+let mobile_nav_items    = document.querySelectorAll('.mobile-nav .nav-items') 
 
 mobile_close.addEventListener('click', function(){
     mobile_nav.classList.remove('none')
@@ -10,3 +11,11 @@ mobile_close.addEventListener('click', function(){
 burger.addEventListener('click', function() {
     mobile_nav.classList.remove('none')
 })
+
+
+for(let mobile_nav_item of mobile_nav_items) {
+    mobile_nav_item.addEventListener('click', () => {
+        mobile_nav.classList.remove('none')
+        mobile_nav.classList.add('none')
+    })
+}
